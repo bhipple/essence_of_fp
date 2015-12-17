bc09737a650d61c0fab65ad132031e0e9c3c485d
Benjamin Hipple <brh2113@columbia.edu>
2015-12-17 17:45:31 -0500

    With Error Monad

--- lambda_calculus.hs
+++ lambda_calculus.hs
@@ -41,16 +41,17 @@ interp (App t u) e = interp t e `bindM` (\f ->
                      apply f a))
 
 lookup :: Name -> Environment -> M Value
-lookup x [] = unitM Wrong
+lookup x [] = errorE ("unbound variable: " ++ x)
 lookup x ((y,b):e) = if x == y then unitM b else Main.lookup x e
 
 add :: Value -> Value -> M Value
 add (Num i) (Num j) = unitM (Num (i+j))
-add a b = unitM Wrong
+add a b = errorE ("should be numbers: " ++ showval a
+                  ++ ", " ++ showval b)
 
 apply :: Value -> Value -> M Value
 apply (Fun k) a = k a
-apply f a = unitM Wrong
+apply f a = errorE ("should be function: " ++ showval f)
 
 test :: Term -> String
 test t = showM (interp t [])
@@ -66,10 +67,33 @@ unitI a = a
 a `bindI` k = k a
 showI = showval
 
-type M a = I a
-unitM = unitI
-bindM = bindI
-showM = showI
+-- type M a = I a
+-- unitM = unitI
+-- bindM = bindI
+-- showM = showI
+
+-------------------------
+-- 1) With Error Messages
+-- This only requires the invalid cases of
+-- lookup, add, and apply substituting
+-- errorE instead of Wrong, and adding some new
+-- data types. This simulates error handling that
+-- we could get with exceptions or continuations.
+data E a = Success a | Error String
+
+unitE = Success
+errorE = Error
+
+(Success a) `bindE` k = k a
+(Error s) `bindE` k = Error s
+
+showE (Success a) = "Success: " ++ showval a
+showE (Error s) = "Error: " ++ s
+
+type M a = E a
+unitM = unitE
+bindM = bindE
+showM = showE
 
 ----------------------
 -- To be used as a test:
@@ -77,4 +101,10 @@ showM = showI
 term0 = App (Lam "x" (Add (Var "x") (Var "x")))
             (Add (Con 10) (Con 11))
 
-main = putStrLn . test $ term0
+-- Invalid syntax: Trying to apply integers
+term1 = App (Con 1) (Con 2)
+
+main = do
+        putStrLn "Running tests:"
+        mapM_ (putStrLn . test) [term0, term1]
+        putStrLn "Done!"
